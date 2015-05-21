aeb29fdba8c91d1a6fb6cfcdd512b9ded8314f3e
Masatoshi Nishimura <massa_n@yahoo.ca>
2015-05-21 18:39:04 -0400

    problem3: largest prime factor[cleaned up]

--- pb3_primefactor.js
+++ pb3_primefactor.js
@@ -2,7 +2,6 @@ function findPrimeFactor (factors) {
 	var division = 2;
 	var primeFactors = [];
 	while(factors>1){
-		// console.log(factors);
 		if (factors/division % 1 === 0) {
 			factors /=division;	
 			primeFactors.push(division)
@@ -10,9 +9,7 @@ function findPrimeFactor (factors) {
 			division++;
 		};
 	}
-	console.log(primeFactors);
-	return factors;
+	return primeFactors;
 }
 
 console.log(findPrimeFactor(600851475143));
-//console.log(4/3%1 ===0)

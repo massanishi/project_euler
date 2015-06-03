bac21cbe08ccdeab926fcd86b5f3220b4b32fe56
Masatoshi Nishimura <massa_n@yahoo.ca>
2015-06-02 22:51:25 -0400

    optimized pb7 using squareroot and prime division

--- pb7_primenumber.js
+++ pb7_primenumber.js
@@ -1,7 +1,8 @@
 console.time("primecount");
+var primeContainer = [];
 function findNthPrime (n){
 
-	var primeContainer = [], i = 2;
+	var i = 2;
 	while(primeContainer.length<n){
 		if(checkForPrime(i)) primeContainer.push(i);
 		i++;
@@ -9,11 +10,21 @@ function findNthPrime (n){
 	return primeContainer[primeContainer.length-1];
 }
 
+// Old one just for record. It was taking 9sec
+// function checkForPrime (num) {
+// 	for (var i = 2; i <= num; i++) {
+
+// 		if((num/i)%1 === 0){
+// 			return false;
+// 		}
+// 	};
+// 	return true;
+// }
 
 function checkForPrime (num) {
-	for (var i = 2; i < num; i++) {
-
-		if((num/i)%1 === 0){
+	for (var i = 0; primeContainer[i] <= Math.sqrt(num); i++) {
+		
+		if((num/primeContainer[i])%1 === 0){
 			return false;
 		}
 	};
